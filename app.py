4f5e6a47207f4046bca7ef436a9214984fe9fa38
Mark <yhc_fb@sina.com>
2024-09-03 16:50:46 +0800

    session改后端存储+简单文章润色

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import sys
 from flask import Flask
 from flask_cors import CORS
+from flask_session import Session
 
 # 添加项目根目录到Python路径
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
@@ -10,6 +11,14 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
     app.config.from_object('config.Config')
+    app.config['SESSION_TYPE'] = 'filesystem'  # 使用文件系统来存储 session 数据
+    app.config['SESSION_FILE_DIR'] = './flask_session/'  # 指定 session 文件存储的目录
+    app.config['SESSION_PERMANENT'] = False  # 如果为 False，则关闭浏览器后 session 失效
+    app.config['SESSION_USE_SIGNER'] = True  # 让 session ID 使用签名机制更安全
+    app.config['SESSION_KEY_PREFIX'] = 'myapp_'  # session 数据前缀
+
+    # 初始化 session
+    Session(app)
 
     with app.app_context():
         from routes import main_bp, translate_bp, md2all_bp, speech2text_bp, article_bp
