32d2a7591b32bc754be70dadb81c29e7d03048c1
FB208 <yhc_fb@sina.com>
2025-01-09 00:35:05 +0800

    微信机器人改为谷歌搜索ai

--- services/wechat_service.py
+++ services/wechat_service.py
@@ -3,7 +3,7 @@ from llm.llm_factory import LLMFactory
 from utils.mem0ai_util import query as mem0ai_query,add as mem0ai_add
 
 def simple_reply(sender_nickname,content):
-    system_prompt = """你是一个拟人化的问答机器人，基于别人跟你说的话，给出人性化的回复"""
+    system_prompt = """你是一个博学且幽默的人，正在使用wechat聊天工具和人或在群聊中与人聊天，你将得到和你对话的人的简介和他跟你说的话，你会像朋友聊天一样回复他"""
     messages = [
         {"role": "system", "content": system_prompt},
         
@@ -18,14 +18,17 @@ def simple_reply(sender_nickname,content):
             if 'memory' in result and result['memory']:
                 assistant_msg = {
                     "role": "assistant",
-                    "content": f"摘要{index}：{result['memory']}"
+                    "content": f"聊天对象的简介{index}：{result['memory']}"
                 }
                 messages.append(assistant_msg)
     question = {"role": "user", "content": content}
     messages.append(question)
-    llm_service = LLMFactory.get_llm_service()
-    completion = llm_service.get_chat_completion(messages)
-    ai_response = llm_service.get_messages(completion)
+    llm_service = LLMFactory.get_llm_service("gemini")
+    completion = llm_service.get_search_chat_completion(messages)
+    eachs = []
+    for each in completion.candidates[0].content.parts:
+        eachs.append(each.text)
+    ai_response = ";".join(eachs)
     mem0_msg = [
         {
             'role':'user',
